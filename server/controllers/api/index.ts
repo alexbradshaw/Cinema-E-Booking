d782315e27a836a943636cb5ed4f4ec043da4f74
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-19 02:32:17 -0500

    added exports for controllers

---
+++ server/controllers/api/index.ts
@@ -0,0 +1,4 @@
+import { authCheck, login, logout, signup } from "./authController";
+import { addMovie, search } from "./movieController";
+
+export { addMovie, authCheck, login, logout, search, signup } 
