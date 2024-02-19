75a23a299000ade1f896029f786da59cab0a654e
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-19 16:53:29 -0500

    fixed routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Home from './views/HomePage';
-import Register from './views/Register';
-import RegisterConfirmation from './views/RegisterConfirmation';
-import Login from './views/Login';
-import EditProfile from './views/EditProfile';
-import Booking from './views/Booking';
-import OrderSummary from './views/OrderSummary';
-import Checkout from './views/Checkout';
-import OrderConfirmation from './views/OrderConfirmation';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" component={Home} />
-        {/*
-        <Route path="/register" component={Register} />
-        <Route path="/registerConfirmation" component={RegisterConfirmation} />
-        <Route path="/login" component={Login} />
-        <Route path="/editProfile" component={EditProfile} />
-        <Route path="/booking" component={Booking} />
-        <Route path="/orderSummary" component={OrderSummary} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orderConfirmation" component={OrderConfirmation} />
-        */}
-      </Routes>
-    </Router>
+    <div className='app'>
+      <Outlet />
+    </div>
   );
 };
 
