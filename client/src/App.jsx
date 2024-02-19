932325dafc6c7ec4b4e8ba00c2102e37ef6c781e
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-02-19 11:21:54 -0500

    added views

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Home from './views/home';
+import Register from './views/Register';
+import RegisterConfirmation from './views/RegisterConfirmation';
+import Login from './views/Login';
+import EditProfile from './views/EditProfile';
+import Booking from './views/Booking';
+import OrderSummary from './views/OrderSummary';
+import Checkout from './views/Checkout';
+import OrderConfirmation from './views/OrderConfirmation';
+
+const App = () => {
   return (
-    <div className='app'>
-        <div>hi</div>
-    </div>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/registerConfirmation" component={RegisterConfirmation} />
+        <Route path="/login" component={Login} />
+        <Route path="/editProfile" component={EditProfile} />
+        <Route path="/booking" component={Booking} />
+        <Route path="/orderSummary" component={OrderSummary} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/orderConfirmation" component={OrderConfirmation} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/views/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Currently Running Movies</h1>
+      {/* Display currently running movies here */}
+
+      <h1>Coming Soon Movies</h1>
+      {/* Display coming soon movies here */}
+
+      <h2>Search for a Movie</h2>
+      <form action="/search" method="get">
+        <label htmlFor="title">Title:</label>
+        <input type="text" name="title" id="title" required />
+        <button type="submit">Search</button>
+      </form>
+
+      <h2>Login</h2>
+      <a href="/login">Login</a>
+
+      <script>
+        {/* Embed movie trailers using external media (e.g., YouTube) */}
+        {/* Implement logic to play trailers */}
+      </script>
+    </div>
+  );
+};
+
+export default Home;
