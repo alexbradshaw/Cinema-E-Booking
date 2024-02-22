b23f86d2b400d6f8d54a312c6c447187f51b92c6
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-22 14:39:13 -0500

    added exports

---
+++ server/seeds/index.ts
@@ -0,0 +1,11 @@
+import categories from './categories.json';
+import members from './members.json';
+import movieCategories from './moviecategories.json';
+import movies from './movies.json';
+import persons from './persons.json';
+import promotions from './promotions.json';
+import tickets from './tickets.json';
+import transactions from './transactions.json';
+import users from './users.json';
+
+export { categories, members, movieCategories, movies, persons, promotions, tickets, transactions, users }
