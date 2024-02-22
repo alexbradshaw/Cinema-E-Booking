2c2d39e731d7efaeca248cdc9177054eae1b40c7
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-22 14:40:28 -0500

    set up seeding for each model

--- server/seeds/seed.ts
+++ server/seeds/seed.ts
@@ -1,19 +1,38 @@
 import sequelize from '../config/connection';
-import { Movie, User } from '../models'
 
-import users from './users.json';
-import movies from './movies.json';
+import { Category, CastMember, MovieCategory, Movie, Person, Promotion, Ticket, Transaction, User } from '../models';
+import { categories, members, movieCategories, movies, persons, promotions, tickets, transactions, users } from './';
 
 const seed = async () => {
     try {
-        await sequelize.sync();
-        await User.bulkCreate(users)
+        await sequelize.sync({force: true});
 
-        console.log("Users Seeded");
+        await Person.bulkCreate(persons);
+        console.log("Persons Seeded");
 
         await Movie.bulkCreate(movies);
-
         console.log("Movies Seeded");
+
+        await User.bulkCreate(users);
+        console.log("Users Seeded");
+
+        await Category.bulkCreate(categories);
+        console.log("Categories Seeded");
+
+        await CastMember.bulkCreate(members);
+        console.log("Person Join Table Seeded");
+
+        await MovieCategory.bulkCreate(movieCategories);
+        console.log("Category Join Table Seeded");
+
+        await Promotion.bulkCreate(promotions);
+        console.log("Promotions Seeded");
+
+        await Transaction.bulkCreate(transactions);
+        console.log("Transactions Seeded");
+
+        await Ticket.bulkCreate(tickets);
+        console.log("Tickets Seeded");
 
         process.exit(0);
     } catch (e) {
