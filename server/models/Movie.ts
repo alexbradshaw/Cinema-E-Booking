4fd52418096e3db77f8f92bee8f71e0de1852b6d
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-22 14:45:06 -0500

    updated model to have starts and stop showing fields, added a synopsis field

--- server/models/Movie.ts
+++ server/models/Movie.ts
@@ -1,5 +1,6 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from "../config/connection";
+import Person from './Person';
 
 class Movie extends Model {
     declare id: number;
@@ -9,6 +10,10 @@ Movie.init(
   {
     title: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    synopsis: {
+      type: DataTypes.TEXT,
       allowNull: false,
     },
     length: {
@@ -27,9 +32,39 @@ Movie.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    coming_soon: {
-      type: DataTypes.BOOLEAN,
+    starts_showing: {
+      type: DataTypes.DATE,
+      defaultValue: () => {
+        const date = new Date();
+        date.setMonth(date.getMonth() +  1)
+        return date.getTime();
+      },
       allowNull: false,
+    },
+    stops_showing: {
+      type: DataTypes.DATE,
+      defaultValue: () => {
+        const date = new Date();
+        date.setMonth(date.getMonth() +  2)
+        return date.getTime();
+      },
+      allowNull: false,
+    },
+    director_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Person,
+        key: 'id'
+      }
+    },
+    producer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Person,
+        key: 'id'
+      }
     },
   },
   {
