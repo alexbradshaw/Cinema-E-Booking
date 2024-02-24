8e396161529b340ead89f51e431a3a3054fab6e2
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-24 15:11:13 -0500

    added context provider and implemented reducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { createContext, useReducer } from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from './views/components/Navbar';
+
+export const AuthContext = createContext();
+
+const initialState = {
+  admin: {
+    isAdmin: false,
+  },
+  auth: false,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'RESET':
+      return initialState;
+    case 'SET_ADMIN':
+      return { ...state, admin: action.payload };
+    case 'SET_AUTH':
+      return { ...state, auth: action.payload };
+    default:
+      return state;
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className='app'>
-      <Outlet />
+      <AuthContext.Provider value={{ ...state, dispatch }}>
+        <Navbar/>
+        <Outlet />
+      </AuthContext.Provider>
     </div>
   );
 };
