bfe9a66d88dd144f54ffe0eecb641a4430214c58
alexbradshaw <alexanderbradshaw5@gmail.com>
2024-02-24 19:34:22 -0500

    update movie trailer to be embedded

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -51,9 +51,7 @@ const Home = () => {
                     <span>{movie.coming_soon ? 'Coming Soon' : 'Currently Running'}</span>
                   </div>
                   <div className="trailerLinkBar">
-                    <a href={movie.trailer_url} target="_blank" rel="noopener noreferrer">
-                      Watch Trailer
-                    </a>
+                    <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + movie.trailer_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                   </div>
                 </div>
               </div>
