963af759ead79ac4994de5b9063571c2d429b9c1
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-02-26 10:21:06 -0500

    added booking to movie page

--- client/src/views/MoviesPage.jsx
+++ client/src/views/MoviesPage.jsx
@@ -1,9 +1,11 @@
 // MoviePage.jsx
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import './CSS/MoviesPage.css';
 
 const MoviePage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { results } = location.state || {};
 
   // Ensure that there are search results before rendering
@@ -18,6 +20,11 @@ const MoviePage = () => {
       <img src={results[0].poster_url} alt={results[0].title} />
       <p>{results[0].synopsis}</p>
       {/* Add more details as needed */}
+
+      {/* Button to navigate to /booking */}
+      <button class="bookbutton" onClick={() => navigate('/booking')}>
+        Book Now
+      </button>
     </div>
   );
 };
