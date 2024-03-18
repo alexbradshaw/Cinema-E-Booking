7ad5d9b91fb9b4ba8d07bb54d091f474d8cad87f
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2024-03-17 20:16:59 -0400

    added email util functions

---
+++ server/utils/utils.ts
@@ -0,0 +1,46 @@
+import mail from 'nodemailer';
+import { User } from '../models';
+
+const transporter = mail.createTransport({
+    host: 'smtp-relay.brevo.com', 
+    port: 587,
+    secure: false, 
+    auth: {
+      user: 'thisismyspamemail2014@gmail.com', 
+      pass: 'dM57vTyK1ascjk9G', 
+    },
+});
+
+export const sendConfirmEmail = async (target: string, user: User) => {
+    const token = user.token_identifier;
+    const prefix = process.env.PRODUCTION ? process.env.APP_URL : 'localhost:3000';
+
+    await transporter.sendMail({
+        from: '"Cinema E-Booking" <thisismyspamemail2014@gmail.com>', 
+        to: target, 
+        subject: "Confirm Email - Cinema E-Booking", 
+        text: "Thank your for signing up for our Cinema Booking Website!", 
+        html: `<div><a href='${prefix}/verify/${token}'>Click here to verify your account.</a></div>`, 
+    });
+}
+
+export const sendResetEmail = async (target: string, token: string) => {
+    const prefix = process.env.PRODUCTION ? process.env.APP_URL : 'localhost:3000';
+
+    await transporter.sendMail({
+        from: '"Cinema E-Booking" <thisismyspamemail2014@gmail.com>', 
+        to: target, 
+        subject: "Reset Password - Cinema E-Booking", 
+        text: "Someone requested a password reset for this email, it will expire in 30 minutes. If you did not request this, you can safely disregard this email.", 
+        html: `<div><a href='${prefix}/reset/${token}'>Reset Password Here</a></div>`, 
+    });
+}
+
+export const sendUpdateEmail = async (target: string | undefined, username: string | undefined, item: string) => {
+    await transporter.sendMail({
+        from: '"Cinema E-Booking" <thisismyspamemail2014@gmail.com>', 
+        to: target, 
+        subject: "Account Information Changed", 
+        text: `This is a notification that your account ${username}${item} was recently updated on our site. If this was not done by you, please contact support immediately.`, 
+    });
+}
