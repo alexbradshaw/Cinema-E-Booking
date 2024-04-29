f38a9f6eb9ef2d2c3d465ad83ab435daa222fd0f
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2024-04-29 00:39:54 -0400

    implemented react query, should be virtually the same

--- client/src/views/ChangePassword.jsx
+++ client/src/views/ChangePassword.jsx
@@ -1,64 +1,59 @@
 import React, { useState } from 'react';
 import { changePassword } from '../utils/API';
 import { Link, useParams } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
 const ChangePassword = () => {
   const [oldPassword, setOld] = useState('');
   const [password, setPassword] = useState('');
-  const [submitted, setSubmitted] = useState(false);
   const { token } = useParams();
+
+  const changePass = useMutation({ mutationFn: changePassword })
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    try {
-      if (password.length < 4) {
-        throw new Error('Password is too short!');
-      }
-      await changePassword(token, password);
-      setSubmitted(true);
-    } catch (error) {
-      console.error('Error during registration:', error);
-
-      alert('Password change failed. ' +  error);
-    }
+    changePass.mutate({ token, password });
   };
 
   return (
     <div>
       {
-        !submitted ?
-        <>
-          <h1>Change Password</h1>
-          <form onSubmit={handleSubmit}>
+        changePass.isError && (<h1>There was an error with changing your password!</h1>)
+      }
+      {
+        changePass.isSuccess 
+          ? 
+            <>
+              <h1>Your password has been updated!</h1>
+              <Link to={'/login'}>Proceed to Login</Link>
+            </>
+            :
+            <>
+              <h1>Change Password</h1>
+              <form onSubmit={handleSubmit}>
 
-            
-            <label htmlFor="oldPassword">Enter your old password:</label>
-            <input
-              type="text"
-              id="oldPassword"
-              value={oldPassword}
-              onChange={(e) => setOld(e.target.value)}
-              required
-            />
+                
+                <label htmlFor="oldPassword">Enter your old password:</label>
+                <input
+                  type="text"
+                  id="oldPassword"
+                  value={oldPassword}
+                  onChange={(e) => setOld(e.target.value)}
+                  required
+                />
 
-            <label htmlFor="password">Enter your new password:</label>
-            <input
-              type="text"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+                <label htmlFor="password">Enter your new password:</label>
+                <input
+                  type="text"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
 
-            <button type="submit">Change Password</button>
-          </form>
-        </>
-        : 
-        <>
-            <h1>Your password has been updated!</h1>
-            <Link to={'/login'}>Proceed to Login</Link>
-        </>
+                <button type="submit">Change Password</button>
+              </form>
+            </>
       }
     </div>
   );
