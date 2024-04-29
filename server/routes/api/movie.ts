09f44e7acecd950c424f915d5c3d1226fc2c0c4e
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2024-04-29 00:33:13 -0400

    added slim movies route

--- server/routes/api/movie.ts
+++ server/routes/api/movie.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { findMovies, searchMovies } from '../../controllers/index.js';
+import { findMovies, findMoviesSlim, searchMovies } from '../../controllers/index.js';
 
 const movies = Router();
 
@@ -11,6 +11,7 @@ const movies = Router();
 
     // GET
     movies.get('/', findMovies);                       // * GET route to find all movies * 
+    movies.get('/slim', findMoviesSlim);                       // * GET route to find all movies * 
     movies.get('/:title', searchMovies);               // * GET route to find a list of movies matching a title * 
 
 export default movies;
