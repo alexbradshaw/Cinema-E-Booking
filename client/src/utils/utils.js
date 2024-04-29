cbbf68f2d2a83598e75b7b29e67e63d3a23e5073
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2024-04-29 00:35:54 -0400

    added time format func

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,3 +2,14 @@ export const formatDate = (date) => {
     const newDate = new Date(date)
     return `${newDate.getUTCMonth() + 1}/${newDate.getUTCDate()}/${newDate.getFullYear()}`
 }
+
+export const formatMovieLength = (length) => {
+    const hours = Math.floor(length / 60);
+    const minutes = length % 60;
+  
+    if (minutes < 10) {
+      return `${hours}:0${minutes}`
+    }
+
+    return `${hours}:${minutes}`
+}
