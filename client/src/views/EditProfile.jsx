57547c5e48925ce56a9d0ac8e5b3d0c82115769a
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-04-30 18:59:14 -0400

    minor change to edit profile

--- client/src/views/EditProfile.jsx
+++ client/src/views/EditProfile.jsx
@@ -250,7 +250,10 @@ const EditProfile = () => {
           }
         </form>
         <div>
-          <label htmlFor="purchases">Purchases</label>
+          <label htmlFor="purchases">Purchases:</label>
+          <div className="purchasesTab"> 
+          
+          </div>
         </div>
 
       </div>
