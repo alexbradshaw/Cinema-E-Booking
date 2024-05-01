38db6f6478636de41125f3988b1ef4767cd2bb67
wcepress4 <wcepress@yahoo.com>
2024-04-30 23:56:48 -0400

    reformatted movie pages

--- client/src/views/MoviesPage.jsx
+++ client/src/views/MoviesPage.jsx
@@ -3,28 +3,44 @@ import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './CSS/MoviesPage.css';
 
+const formatTime = (minutes) => {
+  const hours = Math.floor(minutes / 60);
+  const remainingMinutes = minutes % 60;
+  return `${hours}h ${remainingMinutes}m`;
+};
+
 const MoviePage = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { results } = location.state || {};
 
-  // Ensure that there are search results before rendering
   if (!results) {
     return <p>No results found.</p>;
   }
 
-  // You may want to customize this rendering based on your data structure
-  return (
-    <div>
-      <h1>{results[0].title}</h1>
-      <img src={results[0].poster_url} alt={results[0].title} />
-      <p>{results[0].synopsis}</p>
-      {/* Add more details as needed */}
+  const lengthInMinutes = results[0].length;
 
-      {/* Button to navigate to /booking */}
-      <button class="bookbutton" onClick={() => navigate('/booking')}>
-        Book Now
-      </button>
+  return (
+    <div className="moviePageContainer">
+      <div className="posterContainer">
+        <img className="poster" src={results[0].poster_url} alt={results[0].title} />
+      </div>
+      <div className="movieDetails">
+        <h1 className="title">{results[0].title}</h1>
+        <p className="detailsText">
+          {results[0].rating} &nbsp; &nbsp;
+          {formatTime(lengthInMinutes)} &nbsp; &nbsp;
+          <span className="director">Directed by:</span> {results[0].Director.name}
+        </p>
+        <p className="detailsText">{results[0].synopsis}</p>
+        
+        <h1>Showtimes</h1>
+        {/* Showtimes here */}
+
+        <button className="bookButton" onClick={() => navigate('/booking')}>
+          Book Now
+        </button>
+      </div>
     </div>
   );
 };
